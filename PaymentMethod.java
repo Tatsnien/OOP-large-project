aeb1947f855331e5dc471cc87556fe1f6fa1481b
Nlda191105 <98066207+nldahust@users.noreply.github.com>
2024-12-04 21:34:56 +0700

    Add files via upload

---
+++ PaymentMethod.java
@@ -0,0 +1,29 @@
+package Payment;
+
+public class PaymentMethod {
+	private String ownername;
+	private long number;
+	private double balance;
+	private String issuer;
+
+	public PaymentMethod() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getOwnername() {
+		return ownername;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+}
