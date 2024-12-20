dda6128cd50719f6b7f5a7e5d59a80c59010748e
Nlda191105 <98066207+nldahust@users.noreply.github.com>
2024-12-20 16:07:39 +0700

    Update SalaryExpense.java

--- OOPs/src/data/SalaryExpense.java
+++ OOPs/src/data/SalaryExpense.java
@@ -1,8 +1,13 @@
 package data;
 
-public class SalaryExpense extends Expense{
-  private Personnel personnel;
-  private float salary;
+public class SalaryExpense extends Expense {
+    private Personnel personnel;
+    private float salary;
    
-public SalaryExpense(
+public SalaryExpense(String expenseName, double expenseValue, Personnel personnel, float salary) {
+    super(expenseName, expenseValue);
+    this.personnel = personnel;
+    this.salary = salary  
+  }
+
 }
