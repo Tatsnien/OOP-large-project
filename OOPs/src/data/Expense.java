10a83a187ffe8dd9cb8f0cb1bdb76c278843e13b
Nlda191105 <98066207+nldahust@users.noreply.github.com>
2024-12-20 15:46:45 +0700

    Update Expense.java

--- OOPs/src/data/Expense.java
+++ OOPs/src/data/Expense.java
@@ -1,5 +1,19 @@
-package data;
+package Data;
 
 public class Expense {
+	private String expenseName;
+	private double expenseValue;
+	
+	
+	
+
+
+	public Expense(String expenseName, double expenseValue) {
+		this.expenseName= expenseName;
+		this.expenseValue= expenseValue;
+		// TODO Auto-generated constructor stub
+	}
+
+	
 
 }
