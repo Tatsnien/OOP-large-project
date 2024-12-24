3041abff6a36e7cfb947a6e91b976027eccdc67e
Nlda191105 <98066207+nldahust@users.noreply.github.com>
2024-12-24 10:30:38 +0700

    Update PercentageDiscount.java with Constructors

--- OOPs/src/data/PercentageDiscount.java
+++ OOPs/src/data/PercentageDiscount.java
@@ -2,11 +2,18 @@ package data;
 
 class PercentageDiscount extends Discount {
 	private float percent;
-
+	
 	public PercentageDiscount(Date startDate, Date endDate, Item[] appliedItem, float percent) {
 		super(startDate, endDate, appliedItem);
 		this.percent = percent;
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
+	public PercentageDiscount(float percent) {  
+		this.percent= percent;
+	}
+	
+	
 }
